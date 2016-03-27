b69f8d81c3632c732ff05740087aa62c910d9235
Marcus Rugger <marcusrugger@yahoo.com>
2016-03-26 20:00:36 -0400

    Working with libraries

--- src/game-bot.ino
+++ src/game-bot.ino
@@ -1,12 +1,39 @@
-#include <Arduino.h>
-#include <functional-bot.h>
-#include <functional-vlpp.h>
 #include <StandardCplusplus.h>
+#include <functional-vlpp.h>
+#include <functional-bot.h>
+#include "build.robotfabricator.h"
+
+
+static Idleloop idleloop;
+
+
+static void createAnimator(BuildFabricator &fabricator)
+{
+    fabricator.buildDisplayAnimator();
+}
+
+
+static void createWeatherStation(BuildFabricator &fabricator)
+{
+    fabricator.buildDisplayPin();
+    // fabricator.buildDisplayAnimator();
+    // fabricator.buildMatrixAnimator();
+}
 
 
 void setup(void)
-{}
+{
+    Serial.begin(9600);
+
+    RobotFabricator fabricator;
+    createWeatherStation(fabricator);
+    idleloop = fabricator.getIdleloop();
+
+    Serial.println("Setup complete");
+}
 
 
 void loop(void)
-{}
+{
+    idleloop();
+}
