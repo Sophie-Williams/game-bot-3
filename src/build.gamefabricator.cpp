f2ebd839f6b3730d4e6f17cbba48d96840a6dc2e
Marcus Rugger <marcusrugger@yahoo.com>
2016-03-29 06:40:10 -0400

    Assemble Me4Button

--- src/build.gamefabricator.cpp
+++ src/build.gamefabricator.cpp
@@ -1,13 +1,28 @@
 #include "build.gamefabricator.h"
 #include "subject.me4button.h"
+#include "subject.me1button.h"
 
 
 GameFabricator::GameFabricator(void)
 {}
 
 
-Me4ButtonSubject GameFabricator::assembleMe4Button(uint8_t pinNumber)
+Me4ButtonSubject *GameFabricator::assembleMe4Button(uint8_t pinNumber)
 {
     ControllerPin *pin = new ControllerPin(pinNumber);
-    return Me4ButtonSubject(pin);
+    _subject4Button = new Me4ButtonSubject(pin);
+
+    _subjectKey1    = new Me1ButtonSubject(*_subject4Button, Me4ButtonSubject::BUTTON_1);
+    _subjectKey2    = new Me1ButtonSubject(*_subject4Button, Me4ButtonSubject::BUTTON_2);
+    _subjectKey3    = new Me1ButtonSubject(*_subject4Button, Me4ButtonSubject::BUTTON_3);
+    _subjectKey4    = new Me1ButtonSubject(*_subject4Button, Me4ButtonSubject::BUTTON_4);
+    _subjectKeyNone = new Me1ButtonSubject(*_subject4Button, Me4ButtonSubject::BUTTON_NONE);
+
+    _subject4Button->attach(*_subjectKey1);
+    _subject4Button->attach(*_subjectKey2);
+    _subject4Button->attach(*_subjectKey3);
+    _subject4Button->attach(*_subjectKey4);
+    _subject4Button->attach(*_subjectKeyNone);
+
+    return _subject4Button;
 }
--- src/build.gamefabricator.h
+++ src/build.gamefabricator.h
@@ -4,6 +4,7 @@
 #include <build.robotfabricator.h>
 
 class Me4ButtonSubject;
+class Me1ButtonSubject;
 
 
 class GameFabricator : public RobotFabricator
@@ -15,8 +16,17 @@ public:
 
 protected:
 
-    Me4ButtonSubject assembleMe4Button(uint8_t pinNumber);
+    Me4ButtonSubject *assembleMe4Button(uint8_t pinNumber);
 
+
+private:
+
+    Me4ButtonSubject    *_subject4Button;
+    Me1ButtonSubject    *_subjectKey1;
+    Me1ButtonSubject    *_subjectKey2;
+    Me1ButtonSubject    *_subjectKey3;
+    Me1ButtonSubject    *_subjectKey4;
+    Me1ButtonSubject    *_subjectKeyNone;
 };
 
 
