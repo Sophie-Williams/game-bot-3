91e93970055ae1e73d64bc77d6f88381d36b1ad4
Marcus Rugger <marcusrugger@yahoo.com>
2016-04-06 17:54:18 -0400

    More refactoring for subjects

--- src/build.gamefabricator.cpp
+++ src/build.gamefabricator.cpp
@@ -10,23 +10,20 @@ GameFabricator::GameFabricator(void)
 
 Me4ButtonSubject *GameFabricator::assembleMe4Button(uint8_t pinNumber)
 {
-    Notifier notice4Button;
-    Notifier notice1Button;
-
     ControllerPin *pin = new ControllerPin(pinNumber);
-    _subject4Button    = new Me4ButtonSubject(notice4Button, pin);
+    _subject4Button    = new Me4ButtonSubject(_notice4Button, pin);
 
-    _subjectKey1    = new Me1ButtonSubject(notice1Button, *_subject4Button, Me4ButtonSubject::BUTTON_1);
-    _subjectKey2    = new Me1ButtonSubject(notice1Button, *_subject4Button, Me4ButtonSubject::BUTTON_2);
-    _subjectKey3    = new Me1ButtonSubject(notice1Button, *_subject4Button, Me4ButtonSubject::BUTTON_3);
-    _subjectKey4    = new Me1ButtonSubject(notice1Button, *_subject4Button, Me4ButtonSubject::BUTTON_4);
-    _subjectKeyNone = new Me1ButtonSubject(notice1Button, *_subject4Button, Me4ButtonSubject::BUTTON_NONE);
+    _subjectKey1    = new Me1ButtonSubject(_noticeButton1,    Me4ButtonSubject::BUTTON_1);
+    _subjectKey2    = new Me1ButtonSubject(_noticeButton2,    Me4ButtonSubject::BUTTON_2);
+    _subjectKey3    = new Me1ButtonSubject(_noticeButton3,    Me4ButtonSubject::BUTTON_3);
+    _subjectKey4    = new Me1ButtonSubject(_noticeButton4,    Me4ButtonSubject::BUTTON_4);
+    _subjectKeyNone = new Me1ButtonSubject(_noticeButtonNone, Me4ButtonSubject::BUTTON_NONE);
 
-    notice4Button.subscribe(*_subjectKey1);
-    notice4Button.subscribe(*_subjectKey2);
-    notice4Button.subscribe(*_subjectKey3);
-    notice4Button.subscribe(*_subjectKey4);
-    notice4Button.subscribe(*_subjectKeyNone);
+    _notice4Button.subscribe(*_subjectKey1);
+    _notice4Button.subscribe(*_subjectKey2);
+    _notice4Button.subscribe(*_subjectKey3);
+    _notice4Button.subscribe(*_subjectKey4);
+    _notice4Button.subscribe(*_subjectKeyNone);
 
     return _subject4Button;
 }
--- src/build.gamefabricator.h
+++ src/build.gamefabricator.h
@@ -2,6 +2,7 @@
 #define BUILD_GAMEFABRICATOR_H
 
 #include <build.robotfabricator.h>
+#include <task.notifier.h>
 
 class Me4ButtonSubject;
 class Me1ButtonSubject;
@@ -20,6 +21,13 @@ protected:
 
 
 private:
+
+    Notifier<Me4ButtonSubject> _notice4Button;
+    Notifier<Me1ButtonSubject> _noticeButtonNone;
+    Notifier<Me1ButtonSubject> _noticeButton1;
+    Notifier<Me1ButtonSubject> _noticeButton2;
+    Notifier<Me1ButtonSubject> _noticeButton3;
+    Notifier<Me1ButtonSubject> _noticeButton4;
 
     Me4ButtonSubject    *_subject4Button;
     Me1ButtonSubject    *_subjectKey1;
