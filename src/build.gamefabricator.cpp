3a725024594dbe2e4b566a7fdf590bfa96beec86
Marcus Rugger <marcusrugger@yahoo.com>
2016-04-06 20:40:17 -0400

    Make Me4Button even more composable

--- src/build.gamefabricator.cpp
+++ src/build.gamefabricator.cpp
@@ -10,13 +10,13 @@ GameFabricator::GameFabricator(void)
 
 Runnable GameFabricator::assembleMe4Button(uint8_t pinNumber)
 {
-    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton1,    Me4ButtonSubject::BUTTON_1)    );
-    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton2,    Me4ButtonSubject::BUTTON_2)    );
-    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton3,    Me4ButtonSubject::BUTTON_3)    );
-    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton4,    Me4ButtonSubject::BUTTON_4)    );
-    _notice4Button.subscribe( Me1ButtonSubject(_noticeButtonNone, Me4ButtonSubject::BUTTON_NONE) );
+    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton1,    Me4Button::BUTTON_1)    );
+    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton2,    Me4Button::BUTTON_2)    );
+    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton3,    Me4Button::BUTTON_3)    );
+    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton4,    Me4Button::BUTTON_4)    );
+    _notice4Button.subscribe( Me1ButtonSubject(_noticeButtonNone, Me4Button::BUTTON_NONE) );
 
     ControllerPin pin(pinNumber);
-    Me4ButtonSubject noticePin(_notice4Button);
-    return [pin, noticePin](void) mutable -> void { noticePin(pin.readPin()); };
+    Me4ButtonSubject setButtonState(_notice4Button);
+    return [pin, setButtonState](void) mutable -> void { setButtonState(Me4Button::translatePin(pin.readPin())); };
 }
