cf81321b5ea2d9a19222bb3bb3714352fba65840
Marcus Rugger <marcusrugger@yahoo.com>
2016-04-06 20:08:27 -0400

    Yet, more refactor of the subjects

--- src/build.gamefabricator.cpp
+++ src/build.gamefabricator.cpp
@@ -8,22 +8,15 @@ GameFabricator::GameFabricator(void)
 {}
 
 
-Me4ButtonSubject *GameFabricator::assembleMe4Button(uint8_t pinNumber)
+Runnable GameFabricator::assembleMe4Button(uint8_t pinNumber)
 {
-    ControllerPin *pin = new ControllerPin(pinNumber);
-    _subject4Button    = new Me4ButtonSubject(_notice4Button, pin);
+    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton1,    Me4ButtonSubject::BUTTON_1)    );
+    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton2,    Me4ButtonSubject::BUTTON_2)    );
+    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton3,    Me4ButtonSubject::BUTTON_3)    );
+    _notice4Button.subscribe( Me1ButtonSubject(_noticeButton4,    Me4ButtonSubject::BUTTON_4)    );
+    _notice4Button.subscribe( Me1ButtonSubject(_noticeButtonNone, Me4ButtonSubject::BUTTON_NONE) );
 
-    _subjectKey1    = new Me1ButtonSubject(_noticeButton1,    Me4ButtonSubject::BUTTON_1);
-    _subjectKey2    = new Me1ButtonSubject(_noticeButton2,    Me4ButtonSubject::BUTTON_2);
-    _subjectKey3    = new Me1ButtonSubject(_noticeButton3,    Me4ButtonSubject::BUTTON_3);
-    _subjectKey4    = new Me1ButtonSubject(_noticeButton4,    Me4ButtonSubject::BUTTON_4);
-    _subjectKeyNone = new Me1ButtonSubject(_noticeButtonNone, Me4ButtonSubject::BUTTON_NONE);
-
-    _notice4Button.subscribe(*_subjectKey1);
-    _notice4Button.subscribe(*_subjectKey2);
-    _notice4Button.subscribe(*_subjectKey3);
-    _notice4Button.subscribe(*_subjectKey4);
-    _notice4Button.subscribe(*_subjectKeyNone);
-
-    return _subject4Button;
+    ControllerPin pin(pinNumber);
+    Me4ButtonSubject noticePin(_notice4Button);
+    return [pin, noticePin](void) mutable -> void { noticePin(pin.readPin()); };
 }
--- src/build.gamefabricator.h
+++ src/build.gamefabricator.h
@@ -17,24 +17,18 @@ public:
 
 protected:
 
-    Me4ButtonSubject *assembleMe4Button(uint8_t pinNumber);
+    Runnable assembleMe4Button(uint8_t pinNumber);
 
 
 private:
 
-    Notifier<Me4ButtonSubject> _notice4Button;
-    Notifier<Me1ButtonSubject> _noticeButtonNone;
-    Notifier<Me1ButtonSubject> _noticeButton1;
-    Notifier<Me1ButtonSubject> _noticeButton2;
-    Notifier<Me1ButtonSubject> _noticeButton3;
-    Notifier<Me1ButtonSubject> _noticeButton4;
+    Notifier<Me4ButtonSubject *> _notice4Button;
+    Notifier<Me1ButtonSubject *> _noticeButtonNone;
+    Notifier<Me1ButtonSubject *> _noticeButton1;
+    Notifier<Me1ButtonSubject *> _noticeButton2;
+    Notifier<Me1ButtonSubject *> _noticeButton3;
+    Notifier<Me1ButtonSubject *> _noticeButton4;
 
-    Me4ButtonSubject    *_subject4Button;
-    Me1ButtonSubject    *_subjectKey1;
-    Me1ButtonSubject    *_subjectKey2;
-    Me1ButtonSubject    *_subjectKey3;
-    Me1ButtonSubject    *_subjectKey4;
-    Me1ButtonSubject    *_subjectKeyNone;
 };
 
 
